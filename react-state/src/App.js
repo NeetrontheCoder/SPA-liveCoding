a91d112674411025d4aa2442ee1eb53a69c7453f
Neetu <upneetneetu@gmail.com>
2022-08-08 14:03:57 +0200

    decrement counter

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -1,21 +1,27 @@
-import { useState } from 'react';
-
+import { useState } from "react";
 
 function App() {
-
-
-
-  
   const [count, setCount] = useState(0); //initial value, function
-  
+  const [bgColor, setBgColor] = useState("lightblue");
+
   function incrementCounter() {
     setCount(count + 1);
   }
- 
+
+  function decrementCounter() {
+    setCount(count - 1);
+  }
+
+  function changeBGColor() {
+    setBgColor(bgColor === "lightblue" ? "lightgreen" : "lightblue");
+  }
+
+  console.log("render");
   return (
     <div className="App">
-      <h1>Counter: {count}</h1>
-      <button onClick={incrementCounter}>Click me!</button>
+      <h1 style={changeBGColor}>Counter</h1>
+      <button onClick={() => incrementCounter()}>increment</button>
+      <button onClick={() => decrementCounter()}>decrement</button>
     </div>
   );
 }
